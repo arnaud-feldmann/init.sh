fb64491264334712966fe1373e40ee89cb5dfcf5
arnaud-feldmann <arnaud.feldmann@gmail.com>
2025-02-03 18:34:04 +0100

    branch main

--- init_covapsy.sh
+++ init_covapsy.sh
@@ -1,7 +1,6 @@
 git config --global credential.helper '!f() { printf "%s\n" "username=$GIT_USER_NAME" "password=$GIT_PERSONAL_ACCESS_TOKEN"; };f'
 git clone https://github.com/arnaud-feldmann/covapsy_seti_2024.git
 cd covapsy_seti_2024
-git switch pyro
 cd Webots
 source ./install_dependencies_sspcloud.sh
 
