a7c4a2a868d9091b875b4531150b43b1ef5843f8
arnaud-feldmann <arnaud.feldmann@gmail.com>
2025-04-03 10:18:07 +0200

    init_datasets.sh

---
+++ init_datasets.sh
@@ -0,0 +1,9 @@
+git config --global credential.helper '!f() { printf "%s\n" "username=$GIT_USER_NAME" "password=$GIT_PERSONAL_ACCESS_TOKEN"; };f'
+git clone https://github.com/arnaud-feldmann/generate_dataset.git
+sudo apt update
+sudo apt install libgl1
+cd generate_dataset
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
