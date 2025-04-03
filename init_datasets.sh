bfc41212ecb8a7be9d1492554dd472c9d437a29d
Arnaud Feldmann <60670870+arnaud-feldmann@users.noreply.github.com>
2025-04-03 22:05:42 +0200

    Update init_datasets.sh

--- init_datasets.sh
+++ init_datasets.sh
@@ -1,6 +1,6 @@
 git config --global credential.helper '!f() { printf "%s\n" "username=$GIT_USER_NAME" "password=$GIT_PERSONAL_ACCESS_TOKEN"; };f'
 git clone https://github.com/arnaud-feldmann/generate_dataset.git
-sudo apt update
+sudo apt-get -y update
 sudo apt install libgl1
 cd generate_dataset
 python -m venv .venv
