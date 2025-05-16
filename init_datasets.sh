f353eef927fe23df4efab5661b3ade9f26fdbbe8
Arnaud Feldmann <60670870+arnaud-feldmann@users.noreply.github.com>
2025-05-16 09:53:29 +0200

    Update init_datasets.sh

--- init_datasets.sh
+++ init_datasets.sh
@@ -1,2 +1,4 @@
 git config --global credential.helper '!f() { printf "%s\n" "username=$GIT_USER_NAME" "password=$GIT_PERSONAL_ACCESS_TOKEN"; };f'
 git clone https://github.com/arnaud-feldmann/generate_dataset.git
+sudo apt-get update
+sudo apt-get -y install libgl1
